44c2d72fbeddede67e44a1fb347e33933dacce18
shubham <shubhamsagar628@gmail.com>
2025-07-12 17:30:39 +0530

    Table done with fake data

--- src/Components/FeedPage.jsx
+++ src/Components/FeedPage.jsx
@@ -114,15 +114,17 @@ const FeedPage = () => {
 
 
   return (
-   <div className='min-h-screen p-5'>
-    <table className='w-1/2 md:w-full text-center' border={3}>
-      <thead>
+  
+      <div className='min-h-screen p-5'>
+      <div className="overflow-x-auto"> 
+      <table className='w-full text-center border border-gray-300'>
+      <thead className='bg-gray-200 text-black'>
         <tr className='text-lg'>
-           <th className="px-6 py-3">Title</th>
-              <th className="px-6 py-3">Author</th>
-              <th className="px-6 py-3">ISBN</th>
-              <th className="px-6 py-3">Stock</th>
-              <th className="px-6 py-3">Actions</th>
+           <th className="px-6 py-3 border">Title</th>
+              <th className="px-6 py-3 border">Author</th>
+              <th className="px-6 py-3 border">ISBN</th>
+              <th className="px-6 py-3 border">Stock</th>
+              <th className="px-6 py-3 border">Actions</th>
         </tr>
       </thead> 
       <tbody>
@@ -130,12 +132,12 @@ const FeedPage = () => {
           books.map((book)=>{
             return (
               <tr>
-                <td className="px-6 py-4 hover:cursor-pointer hover:bg-gray-100">{book.title}</td>
-                <td className="px-6 py-4">{book.author}</td>
-                <td className="px-6 py-4">{book.isbn}</td>
-                <td className="px-6 py-4">{book.stock}</td>
-                <td className="px-6 py-4">
-                  <div className='flex gap-2 justify-center'>
+                <td className="px-6 py-4 hover:cursor-pointer hover:bg-gray-200 hover:text-blue-600 font-semibold border">{book.title}</td>
+                <td className="px-6 py-4 border">{book.author}</td>
+                <td className="px-6 py-4 border">{book.isbn}</td>
+                <td className="px-6 py-4 border">{book.stock}</td>
+                <td className="px-6 py-4 border">
+                  <div className='flex flex-col md:flex-row gap-2 justify-center'>
                     <button className="bg-green-600 hover:bg-green-700 text-white px-3 py-1 rounded text-xs hover:cursor-pointer">✏️ Edit</button> 
                     <button className="bg-red-600 hover:bg-red-700 text-white px-3 py-1 rounded text-xs hover:cursor-pointer">🗑️ Delete</button>
                   </div>
@@ -147,7 +149,11 @@ const FeedPage = () => {
         }
       </tbody>
     </table>
+    </div>
+   
    </div>
+ 
+   
   );
 };
 
